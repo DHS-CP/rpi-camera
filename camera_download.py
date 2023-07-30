8addb194fec74179dd68c245ff2a4dcd852e2f4e
Mr. Kaehms <kaehmsrobert@dublinusd.org>
2023-07-30 12:38:44 -0700

    Create camera_download.py
    Original creation of file with notes on what it should do.

---
+++ camera_download.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+#
+# Flask script for managing downloads of camera images.
+#
+# Will do the folowing:
+# 1 Report disk usage
+# 2 Report number of current images available for download
+# 3 Will alow for downloading of files in zip format
+# 4 As part of download, a copy of the zip file will be stored in an archive directory
+# 5 Current set of files before download will be deleted
